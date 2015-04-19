f02b889621ded8de6d646ef5b5ddf1b993903ce0
liangfei <njliangfei@gmail.com>
2015-04-19 19:16:39 +0800

    add myapplication

---
+++ app/src/main/java/com/liangfeizc/masteringfresco/MyApplication.java
@@ -0,0 +1,13 @@
+package com.liangfeizc.masteringfresco;
+
+import android.app.Application;
+
+/**
+ * Created by rufi on 4/19/15.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
