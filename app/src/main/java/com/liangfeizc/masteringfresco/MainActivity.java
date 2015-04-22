1ce78dd09c904b6d4836eebfaed718a72e739aac
feelang <njliangfei@gmail.com>
2015-04-22 19:34:16 +0800

    init seekbar and draweeview

--- app/src/main/java/com/liangfeizc/masteringfresco/MainActivity.java
+++ app/src/main/java/com/liangfeizc/masteringfresco/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
 
@@ -17,22 +18,33 @@ import roboguice.inject.InjectView;
 @ContentView(R.layout.activity_main)
 public class MainActivity extends RoboActionBarActivity {
 
-    @InjectView(R.id.circle_img) SimpleDraweeView img;
-    @InjectView(R.id.aspect_radio) SeekBar aspectRatioSeekBar;
-    @InjectView(R.id.which_fixed) RadioGroup whichFixedRadioGrp;
+    @InjectView(R.id.circle_img)
+    private SimpleDraweeView img;
+
+    @InjectView(R.id.aspect_ratio_bar)
+    private SeekBar aspectRatioSeekBar;
+
+    @InjectView(R.id.which_fixed)
+    private RadioGroup whichFixedRadioGrp;
+
+    @InjectView(R.id.aspect_ratio_value)
+    private TextView aspectRatioValue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         img.setImageURI(Uri.parse(Images.URL));
-        img.setAspectRatio(2f);
+
+        img.setAspectRatio(1);
 
         aspectRatioSeekBar.setMax(9);
+        aspectRatioValue.setText(String.valueOf(aspectRatioSeekBar.getProgress() + 1));
         aspectRatioSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 img.setAspectRatio(progress + 1);
+                aspectRatioValue.setText("" + img.getAspectRatio());
             }
 
             @Override
